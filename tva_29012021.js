8f22b272366ae41eaf0c9ad0839beb91d516c23d
Simon Rossini <simonrossini09.sr@gmail.com>
2021-01-31 17:17:37 +0100

    tp_tva_js_29012021

---
+++ tva_29012021.js
@@ -0,0 +1,72 @@
+var container = {display flex};
+var titre = document.createElement('h2');
+var horstaxe = number;
+var mht = number;
+var montanttva= number; 
+var tauxtva [{20/100=0.2},{19.6/100=0.196},{10/100=0.1},{8.5/100=0.085},{7/100=0.07},{5.5/100=0.055}]
+var montanttva = mht * tauxtva;
+var montanttotal = mht * tauxtva + mht;
+
+titre.append(document.createTextNode('Calculer une TVA'))
+document.querySelector('.container').append(titre);
+
+<br><br><br>;
+
+{	for (index=0; index <= 5; index= ++)
+	<form>
+		<div>
+    		<input type="number" placeholder="Montant hors taxes" aria-label="hors taxes">;
+			<"+">;   	
+    		<input type="number" placeholder="Montant TVA" aria-label="tva">;
+    		<"=">;
+    		<input type="number" placeholder="Montant total" aria-label="ttc">;
+    	</div>
+	</form>
+
+<hr>;
+
+
+
+<form>
+		<div>
+    		<input type="number" placeholder="tva_dispo" aria_label="20%">
+    			document.querySelector('tva').onclick=aria_label;
+    		<input type="number" placeholder="tva_dispo" aria_label="19.6%">
+    			document.querySelector('tva').onclick=aria_label;
+    		<input type="number" placeholder="tva_dispo" aria_label="10%">
+    			document.querySelector('tva').onclick=aria_label;
+    		<input type="number" placeholder="tva_dispo" aria_label="8.5%">
+    			document.querySelector('tva').onclick=aria_label;
+    		<input type="number" placeholder="tva_dispo" aria_label="7%">
+    			document.querySelector('tva').onclick=aria_label;
+    		<input type="number" placeholder="tva_dispo" aria_label="5.5%">
+    			document.querySelector('tva').onclick=aria_label;
+    	</div>
+</form>	
+
+<hr>;
+
+var taux = text align : center;
+	taux= document.querySelector('.container').append(titre);
+	document.write ('Taux TVA') 
+var taux=document.querySelector(tauxtva).onclick
+
+<hr>;
+
+<ol>
+       <p> horstaxe= document.createTextNode ('Montant hors taxes') ++ document.getbyElement('horstaxes');
+		   document.createElementbyID('  ');
+		   document.write(getbyElement)
+		<br>
+		   var tauxtva = document.createElement('table');
+		   montanttva = document.createTextNode ('Montant TVA :') ++ document.getbyElement('tva') ++ createTextNode('avec un taux de TVA égal à') ++ document.getbyElement('tauxtva');
+	</p>
+	<p>		document.createTextNode ('Montant TTC :') 
+			var montanttotal = (horstaxes * tauxtva + horstaxes);
+			var montanttotal = document.createTextNode('Montant total');
+	</p>
+</ol>
+
+}
+
+document.querySelector('.container'); 
